06c6bee71f2bdc5aa689cb77f5279b88ca6986d4
Artem Zhikharev <artemziharew@gmail.com>
2023-03-06 15:53:29 +0300

    First commit my branch

--- zhikarev.cpp
+++ zhikarev.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void findMinMax(int arr[], int size, int &min, int &max) {
+// Первый коммит в свою ветку
+
+void findMinMax(int arr[], int size, int &min, int &max)
+{
     min = max = arr[0]; // Initialize min and max to the first element
 
     // Traverse the array and update min and max as needed
-    for (int i = 1; i < size; i++) {
-        if (arr[i] < min) {
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
             min = arr[i];
-        } else if (arr[i] > max) {
+        }
+        else if (arr[i] > max)
+        {
             max = arr[i];
         }
     }
 }
 
-int main() {
+int main()
+{
     int arr[] = {5, 9, 3, 1, 8, 4, 7};
     int size = sizeof(arr) / sizeof(arr[0]);
     int min, max;
