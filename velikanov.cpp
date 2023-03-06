d17b0d628158cf6f3cd798bb6e00de2b4e688f49
SavedowW <velikanov-em@yandex.ru>
2023-03-06 15:53:57 +0300

    velikanov commit 1

--- velikanov.cpp
+++ velikanov.cpp
@@ -18,6 +18,7 @@ int binarySearch(int arr[], int low, int high, int x) {
 
 int main() {
     int arr[] = { 2, 5, 8, 12, 16, 23, 38, 56, 72, 91 };
+    int commit_velikanov_1;
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 23;
     int result = binarySearch(arr, 0, n - 1, x);
