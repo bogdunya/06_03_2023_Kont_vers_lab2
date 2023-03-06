2130fec3cea05095841ff63a58f265c1d4837530
vikelise <viktoriya.eliseeva.2002@mail.ru>
2023-03-06 15:55:05 +0300

    cat

--- eliseeva.cpp
+++ eliseeva.cpp
@@ -21,3 +21,10 @@ int main() {
   }
   return 0;
 }
+
+cat, but he didn't have to know the truth.
+"I've never met a woman who's not married to some jerk."
+She couldn't stop the smile that spread across her face. "You're right. I'm not a wife. And neither are you."
+He shrugged. "Maybe not, but I'd be the first one to admit I've not been in love."
+His words made her think of her own lack of love. She'd always known how to fall in love, and she'd never been able to find anyone who could replace the man she'd loved and lost.
+Her father.
