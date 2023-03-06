4d0f4eb01b73d7a4322f155aa83fcdda7681aff0
SavedowW <velikanov-em@yandex.ru>
2023-03-06 16:23:09 +0300

    commit velikanov 2

--- velikanov.cpp
+++ velikanov.cpp
@@ -20,6 +20,7 @@ int main() {
     int arr[] = { 2, 5, 8, 12, 16, 23, 38, 56, 72, 91 };
     int commit_velikanov_1;
     int n = sizeof(arr) / sizeof(arr[0]);
+    int commit_velikanov_2;
     int x = 23;
     int result = binarySearch(arr, 0, n - 1, x);
     (result == -1) ? cout << "Element is not present in array"
